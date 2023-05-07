44c46bcbdc243908951f41be0270566a6997eeaa
dxlnr <daniel.illner@outlook.de>
2023-05-08 00:22:01 +0200

    small server update

--- server/api/copilot/ldms.py
+++ server/api/copilot/ldms.py
@@ -3,10 +3,11 @@ import torch
 from diffusers import DiffusionPipeline
 
 
-def txt2img(prompt: str, model_path: str, device: str = 'cuda'):
+def txt2img(prompt: str, model_path: str = "runwayml/stable-diffusion-v1-5", device: str = 'cuda'):
     """Inference for text-to-image model."""
-    p = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+    p = DiffusionPipeline.from_pretrained(model_path)
     p = p.to(device)
-    image = p(prompt).images[0]
+    
+    img = p(prompt).images[0]
 
-    return image
+    return img
--- server/api/copilot/routes.py
+++ server/api/copilot/routes.py
@@ -1,7 +1,8 @@
 """Copilot handling prompts."""
+import io
 import os
 from http import HTTPStatus
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_file
 from flask_cors import cross_origin
 
 from .ldms import txt2img 
@@ -16,8 +17,12 @@ def prompt():
     j = request.get_json()
     p = j['prompt']
 
-    model_path = os.path.join(get_project_root(), 'zoo/models/sd-v1-1-4.ckpt')
-    print(model_path)
-    ing = txt2img(p, model_path, 'cpu') 
+    # model_path = os.path.join(get_project_root(), 'zoo/models/')
+    # print(model_path)
+    pil_img = txt2img(p) 
 
-    return jsonify({'success': 'ok'})
+    # serve the image.
+    img_io = io.BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=100)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
