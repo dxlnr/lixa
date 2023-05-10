91b2fb2a300e3fb936253b50991c41484dcf20a8
dxlnr <daniel.illner@outlook.de>
2023-05-10 21:31:03 +0200

    login form ui finished needs backend func

--- ui/src/routes/login.tsx
+++ ui/src/routes/login.tsx
@@ -5,7 +5,7 @@ import LoginForm from "../components/loginform/loginform";
 const Login: Component = () => {
     return (
     <div class="bg-cover h-screen ">
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-2">
             <LoginForm />
             <div class="bg-[url('./assets/bg.svg')]"></div>
         </div>
