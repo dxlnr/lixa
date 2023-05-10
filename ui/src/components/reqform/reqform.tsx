62180345c6987c16b9f8890b45097dd1c1e7f4db
dxlnr <daniel.illner@outlook.de>
2023-05-11 00:18:47 +0200

    ui request form done

--- ui/src/components/reqform/reqform.tsx
+++ ui/src/components/reqform/reqform.tsx
@@ -1,31 +1,31 @@
 
 const ReqForm = () => {
 
-    return (
-        <div class="items-center justify-center m-4 border-neutral-600 border rounded-full p-2.5">
-        <div class="flex flex-row space-x-2">
-          <div class="bg-opacity-5">
-          <input 
-            type="text" 
-            id="website-admin" 
-            class="rounded-full border border-white text-white focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-md p-2.5" 
-            placeholder=" Name " />
-          </div>
-          <div>
-          <input 
-            type="text" 
-            id="website-admin" 
-            class="rounded-full text-whit block flex-1 min-w-0 w-full text-md p-2.5" 
-            placeholder=" Email " />
-          </div>
-          <div>
-          <button 
-            type="submit" 
-            class="text-white focus:ring-4 focus:outline-none font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-            Join
+    return ( 
+        <div class="m-1">
+          <p 
+            class="text-neutral-500 text-center px-8 text-md"> 
+            Welcome to <strong class="text-white"> the future of contextual content.</strong> Get Early Access. 
+          </p>
+          <div class="items-center justify-center m-4 border-neutral-600 border rounded-full p-2">
+          <div class="flex flex-row space-x-2">
+            <input 
+              type="text" 
+              id="website-admin" 
+              class="appearance-none bg-transparent rounded-full border border-neutral-600 text-white focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-md p-2.5" 
+              placeholder=" Name " />
+            <input 
+              type="text" 
+              id="website-admin" 
+              class="appearance-none bg-transparent rounded-full border border-neutral-600 text-whit block flex-1 min-w-0 w-full text-md p-2.5" 
+              placeholder=" Email " />
+            <button 
+              type="submit" 
+              class="text-black bg-slate-50 bg-opacity-25 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-slate-400 focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              Join Now
             </button>
           </div>
-        </div>
+          </div>
         </div>
     );
 };
