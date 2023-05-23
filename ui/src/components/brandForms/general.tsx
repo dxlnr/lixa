14d7217c7e306811dc29a738a48c30604136dda1
dxlnr <daniel.illner@outlook.de>
2023-05-24 00:48:42 +0200

    create brand carousel looks like something

--- ui/src/components/brandForms/general.tsx
+++ ui/src/components/brandForms/general.tsx
@@ -2,10 +2,10 @@ import { Component } from 'solid-js';
 
 const CreateBrandFormGeneral: Component = () => {
   return (
-    <div class="m-auto p-8">
+    <div class="container m-auto p-8 rounded-lg">
       <div class="flex justify-between sm:inline-flex sm:items-center space-y-2 sm:space-y-0 sm:space-x-3 w-full">
         <input
-          type=""
+          type="text"
           name="name"
           id="name"
           class="py-3 px-1 block w-full outline-none border-b border-gray-200 text-sm focus:border-violet-400"
@@ -15,7 +15,7 @@ const CreateBrandFormGeneral: Component = () => {
       </div>
       <div class="flex justify-between pt-4 sm:inline-flex sm:items-center space-y-2 sm:space-y-0 sm:space-x-3 w-full">
         <input
-          type=""
+          type="text"
           name="location"
           id="location"
           class="py-3 px-1 block w-full outline-none border-b border-gray-200 text-sm focus:border-b focus:border-violet-400"
@@ -25,7 +25,7 @@ const CreateBrandFormGeneral: Component = () => {
       </div>
       <div class="flex justify-between pt-4 sm:inline-flex sm:items-center space-y-2 sm:space-y-0 sm:space-x-3 w-full">
         <input
-          type=""
+          type="text"
           name="profession"
           id="profession"
           class="py-3 px-1 block w-full outline-none border-b border-gray-200 text-sm focus:border-violet-400"
@@ -36,7 +36,7 @@ const CreateBrandFormGeneral: Component = () => {
 
       <div class="flex justify-between pt-4 sm:inline-flex sm:items-center space-y-2 sm:space-y-0 sm:space-x-3 w-full">
         <input
-          type=""
+          type="text"
           name="website"
           id="website"
           class="py-3 px-1 block w-full outline-none border-b border-gray-200 text-sm focus:border-violet-400"
--- ui/src/routes/createBrand.tsx
+++ ui/src/routes/createBrand.tsx
@@ -4,7 +4,7 @@ import Carousel from '../components/carousel/carousel';
 
 const CreateBrand: Component = () => {
   return (
-    <div>
+    <div class="flex flex-col justify-center items-center w-1/2 h-1/2 absolute top-1/4 left-1/4 rounded-lg p-4">
       <Carousel />
     </div>
   );
